e2df73a8447550b2d764269f95cd12a7d58f7223
Malik05255 <mmbbrr99@gmail.com>
2026-09-04 20:29:18 +0300

    Fix duplicate META-INF resources from Google Drive dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,18 @@ android {
         abortOnError = false
     }
 
+    packaging {
+        resources {
+            excludes += setOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+            )
+        }
+    }
+
     buildFeatures {
         compose = true
         buildConfig = true
