24839a3f38eedda516b315143dd53000be9d0e50
Malik05255 <mmbbrr99@gmail.com>
2026-09-04 21:58:04 +0300

    Bump chat database for project account isolation

--- app/src/main/kotlin/com/vibe/app/data/database/ChatDatabaseV2.kt
+++ app/src/main/kotlin/com/vibe/app/data/database/ChatDatabaseV2.kt
@@ -25,7 +25,7 @@ import com.vibe.app.data.database.entity.StringListConverter
         ChatPlatformModelV2::class,
         Project::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = true
 )
 @TypeConverters(
