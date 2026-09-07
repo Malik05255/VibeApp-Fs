a9de4e9e8328ca082a6fad840ac2ce2121ed0110
Malik05255 <mmbbrr99@gmail.com>
2026-09-07 22:32:18 +0300

    feat(h): register reminder tool with H agent

--- app/src/main/kotlin/com/malik/lmai/di/AgentToolModule.kt
+++ app/src/main/kotlin/com/malik/lmai/di/AgentToolModule.kt
@@ -26,6 +26,7 @@ import com.malik.lmai.feature.agent.tool.SearchUiPatternTool
 import com.malik.lmai.feature.agent.tool.GetUiPatternTool
 import com.malik.lmai.feature.agent.tool.GetDesignGuideTool
 import com.malik.lmai.feature.agent.tool.GetProjectMemoTool
+import com.malik.lmai.feature.agent.tool.HReminderTool
 import com.malik.lmai.feature.agent.tool.UpdateProjectIntentTool
 import dagger.Binds
 import dagger.Module
@@ -63,4 +64,5 @@ abstract class AgentToolModule {
     @Binds @IntoSet abstract fun bindGetDesignGuide(tool: GetDesignGuideTool): AgentTool
     @Binds @IntoSet abstract fun bindUpdateProjectIntent(tool: UpdateProjectIntentTool): AgentTool
     @Binds @IntoSet abstract fun bindGetProjectMemo(tool: GetProjectMemoTool): AgentTool
+    @Binds @IntoSet abstract fun bindHReminder(tool: HReminderTool): AgentTool
 }
